69b80a646aca1e9b7f66d80d2ec57db8ca324276
m48483 <71882579+m48483@users.noreply.github.com>
2024-04-01 15:02:15 +0900

    commit

--- style/src/App.jsx
+++ style/src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+import { Button } from "reactstrap";
+
 function App() {
-  return;
-  <>
-    <div className="box"></div>
-    <div className="box"></div>
-  </>;
+  return (
+    <>
+      <div className="box"></div>
+      <div className="box"></div>
+      <Button color="danger">danger</Button>
+    </>
+  );
 }
 
 export default App;
