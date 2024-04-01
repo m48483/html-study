8df889faa3908cd5cbb2e072d3e5fcaed54e2c72
m48483 <71882579+m48483@users.noreply.github.com>
2024-04-01 14:20:20 +0900

    init

---
+++ style/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return;
+  <>
+    <div className="box"></div>
+    <div className="box"></div>
+  </>;
+}
+
+export default App;
